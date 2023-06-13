0770175f31a7a52c5e6df0f7ec28b0c2272e30d9
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-13 09:46:39 +0300

    Using the require function

--- global.js
+++ global.js
@@ -1,3 +1,19 @@
-console.log("Hello World");
+const path = require("path");
+// console.log("Hello World");
 
-global.console.log("Hello World");
+// global.console.log("Hello World");
+
+// let hello = "Hello World from Node.js";
+
+// console.log(global.hello);
+
+// console.log(hello);
+
+console.log(__dirname);
+//console.log(__filename);
+
+console.log(`The file name is ${path.basename(__filename)}`);
+
+for (let key in global) {
+    console.log(key);
+}
