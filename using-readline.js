77653389d9d875e78ed2c6109b290e95d42e9c70
ARBUCHELI <radio_seattle@hotmail.com>
2023-07-03 15:29:46 +0300

    feat: node modules - consuming a module with event emitter

--- using-readline.js
+++ using-readline.js
@@ -7,9 +7,14 @@ const questions = [
 ]
 //The done callback is going to fire, when we are done asquing questions
 
-collectAnswers(questions, (answers) => {
+/** Exercise Consuming a module with EventEmitter.
+ * Because our collectAnswers function now returns an event emitter, we can do this:
+ */
+
+const answerEvents = collectAnswers(questions, (answers) => {
     console.log("Thank you for your answers!");
     console.log(answers);
     process.exit();
 })
 
+answerEvents.on("answer", answer => console.log(`The answer is ${answer}`))
